c0791b34dce576e1794aa8428385e1df9a99e885
Aimee Ralfini <aimee@pixel-palace.net>
2022-08-14 22:55:11 +1200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+document.querySelector('[data-btn]').addEventListener('click',function(){
+const li = document.createElement('li');
+li.textContent = 'country';
+//h1.innerHTML = 'country';
+document.body.appendChild(li)
+})
